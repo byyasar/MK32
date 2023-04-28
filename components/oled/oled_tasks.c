ed6c72bd53ee0a8b117007264c57588e936de204
Yaşar BAYKUŞ <byyasar@gmail.com>
2023-04-28 11:56:34 +0300

    ilk menu yazdırma yapıldı

--- components/oled/oled_tasks.c
+++ components/oled/oled_tasks.c
@@ -59,29 +59,57 @@ int DROP_H = 0;
 int offset_x_batt = 0;
 int offset_y_batt = 0;
 
-
 #define BT_ICON 0x5e
 #define BATT_ICON 0x5b
 #define LOCK_ICON 0xca
 
-//Erasing area from oled
-void erase_area(uint8_t x, uint8_t y, uint8_t w, uint8_t h) {
+// Erasing area from oled
+void erase_area(uint8_t x, uint8_t y, uint8_t w, uint8_t h)
+{
 	u8g2_SetDrawColor(&u8g2, 0);
 	u8g2_DrawBox(&u8g2, x, y, w, h);
 	u8g2_SetDrawColor(&u8g2, 1);
 }
 
-//Function for updating the OLED
-void update_oled(void) {
+// Function for updating the OLED
+void update_oled(void)
+{
 #ifdef BATT_STAT
 	battery_percent = get_battery_level();
 #endif
+	if (xQueueReceive(layer_recieve_q, &curr_layout, (TickType_t)0))
+	{
+		erase_area(0,0,128,32);
+		if (curr_layout == 0)
+		{
+			u8g2_DrawStr(&u8g2, 0, 7, " Win        Ana Menu ");
+			u8g2_DrawStr(&u8g2, 0, 14, " Ent| Esc|-> V- x V+ ");
+			u8g2_DrawStr(&u8g2, 0, 21, "  1 |  2 |  3 | WxM |");
+			u8g2_DrawStr(&u8g2, 0, 28, " Tab|  6 |  7 | Slp |");
+		}
+		else if (curr_layout == 1)
+		{
+			u8g2_DrawStr(&u8g2, 0, 7, " Win        Fusion 360 ");
+			u8g2_DrawStr(&u8g2, 0, 14, " Mod| Esc| -> Scroll");
+			u8g2_DrawStr(&u8g2, 0, 21, " Zom| Rot| Pan| 4 |");
+			u8g2_DrawStr(&u8g2, 0, 28, " Tab|  6 |  7 | 8 |");
+		}
+		else
+		{
+			u8g2_DrawStr(&u8g2, 0, 7, " Win        Ana Menu ");
+			u8g2_DrawStr(&u8g2, 0, 14, " Ent| Esc|-> V- x V+ ");
+			u8g2_DrawStr(&u8g2, 0, 21, "  1 |  2 |  3 | WxM |");
+			u8g2_DrawStr(&u8g2, 0, 28, " Tab|  6 |  7 | Slp |");
+		}
 
-	if (xQueueReceive(layer_recieve_q, &curr_layout, (TickType_t) 0)) {
+		u8g2_SendBuffer(&u8g2);
+	}
+
+	/*if (xQueueReceive(layer_recieve_q, &curr_layout, (TickType_t) 0)) {
 		erase_area(0, 7,127,20);
 		u8g2_SetFont(&u8g2, u8g2_font_8x13_tr);
 		u8g2_DrawStr(&u8g2, 0, 16, layer_names_arr[curr_layout]);
-		u8g2_SendBuffer(&u8g2);
+				u8g2_SendBuffer(&u8g2);
 	}
 	if (xQueueReceive(led_recieve_q, &current_led, (TickType_t) 0)) {
 		erase_area(0, 24, 127, 8);
@@ -131,10 +159,10 @@ void update_oled(void) {
 		}
 		prev_battery_percent = battery_percent;
 
-	}
+	}*/
 }
 
-/*NOTE - 
+/*NOTE -
 erase_area(0, 7,127,20);
 		u8g2_SetFont(&u8g2, u8g2_font_8x13_tr);
 		u8g2_DrawStr(&u8g2, 0, 16, layer_names_arr[curr_layout]);
@@ -142,39 +170,46 @@ erase_area(0, 7,127,20);
 
 */
 
-//oled on connection
-void ble_connected_oled(void) {
+// oled on connection
+void ble_connected_oled(void)
+{
 
 	u8g2_ClearBuffer(&u8g2);
 	u8g2_SetFont(&u8g2, u8g2_font_5x7_tf);
-	u8g2_DrawStr(&u8g2, 0, 6, GATTS_TAG);
-	u8g2_SetFont(&u8g2, u8g2_font_8x13_tr);
-	u8g2_DrawStr(&u8g2, 0, 16, layer_names_arr[current_layout]);
-	u8g2_SetFont(&u8g2, u8g2_font_open_iconic_all_1x_t);
-	u8g2_DrawGlyph(&u8g2, 110 + offset_x_batt , 8 + offset_y_batt, BATT_ICON);
-	u8g2_DrawGlyph(&u8g2, 120 + offset_x_batt, 8 + offset_y_batt , BT_ICON);
+	// u8g2_DrawStr(&u8g2, 0, 6, GATTS_TAG);
+	// u8g2_SetFont(&u8g2, u8g2_font_8x13_tr);
+	u8g2_DrawStr(&u8g2, 0, 7, "Win        Ana Menu ");
+	u8g2_DrawStr(&u8g2, 0, 14, "Ent| Esc|-> V- x V+ ");
+	u8g2_DrawStr(&u8g2, 0, 21, " 1 |  2 |  3 | WxM |");
+	u8g2_DrawStr(&u8g2, 0, 28, "Tab|  6 |  7 | Slp |");
 
-//	if(CHECK_BIT(curr_led,0)!=0){
-//		u8g2_SetFont(&u8g2, u8g2_font_5x7_tf );
-//		u8g2_DrawStr(&u8g2, 0,31,"NUM");
-//		u8g2_SetFont(&u8g2, u8g2_font_open_iconic_all_1x_t );
-//		u8g2_DrawGlyph(&u8g2, 16,32,LOCK_ICON);
-//	}
-//
-//	if(CHECK_BIT(curr_led,1)!=0){
-//		u8g2_SetFont(&u8g2, u8g2_font_5x7_tf );
-//		u8g2_DrawStr(&u8g2, 27,31,"CAPS");
-//		u8g2_SetFont(&u8g2, u8g2_font_open_iconic_all_1x_t );
-//		u8g2_DrawGlyph(&u8g2,48,32,LOCK_ICON);
-//	}
-//	if(CHECK_BIT(curr_led,2)!=0){
-//		u8g2_SetFont(&u8g2, u8g2_font_5x7_tf );
-//		u8g2_DrawStr(&u8g2, 57,31,"SCROLL");
-//		u8g2_SetFont(&u8g2, u8g2_font_open_iconic_all_1x_t );
-//		u8g2_DrawGlyph(&u8g2,88,32,LOCK_ICON);
-//	}
+	// u8g2_DrawStr(&u8g2, 0, 16, layer_names_arr[current_layout]);
 
-	u8g2_SetFont(&u8g2, u8g2_font_5x7_tf);
+	// u8g2_SetFont(&u8g2, u8g2_font_open_iconic_all_1x_t);
+	// u8g2_DrawGlyph(&u8g2, 110 + offset_x_batt , 8 + offset_y_batt, BATT_ICON);
+	// u8g2_DrawGlyph(&u8g2, 120 + offset_x_batt, 8 + offset_y_batt , BT_ICON);
+
+	//	if(CHECK_BIT(curr_led,0)!=0){
+	//		u8g2_SetFont(&u8g2, u8g2_font_5x7_tf );
+	//		u8g2_DrawStr(&u8g2, 0,31,"NUM");
+	//		u8g2_SetFont(&u8g2, u8g2_font_open_iconic_all_1x_t );
+	//		u8g2_DrawGlyph(&u8g2, 16,32,LOCK_ICON);
+	//	}
+	//
+	//	if(CHECK_BIT(curr_led,1)!=0){
+	//		u8g2_SetFont(&u8g2, u8g2_font_5x7_tf );
+	//		u8g2_DrawStr(&u8g2, 27,31,"CAPS");
+	//		u8g2_SetFont(&u8g2, u8g2_font_open_iconic_all_1x_t );
+	//		u8g2_DrawGlyph(&u8g2,48,32,LOCK_ICON);
+	//	}
+	//	if(CHECK_BIT(curr_led,2)!=0){
+	//		u8g2_SetFont(&u8g2, u8g2_font_5x7_tf );
+	//		u8g2_DrawStr(&u8g2, 57,31,"SCROLL");
+	//		u8g2_SetFont(&u8g2, u8g2_font_open_iconic_all_1x_t );
+	//		u8g2_DrawGlyph(&u8g2,88,32,LOCK_ICON);
+	//	}
+
+	/*u8g2_SetFont(&u8g2, u8g2_font_5x7_tf);
 	char buf[sizeof(uint32_t)];
 	snprintf(buf, sizeof(uint32_t), "%d", battery_percent);
 	u8g2_DrawStr(&u8g2, + offset_x_batt, + offset_y_batt, "%");
@@ -182,15 +217,17 @@ void ble_connected_oled(void) {
 		u8g2_DrawStr(&u8g2, + offset_x_batt, 7 + offset_y_batt , buf);
 	} else {
 		u8g2_DrawStr(&u8g2, 85 + offset_x_batt, 7 + offset_y_batt, "100");
-	}
+	}*/
 	u8g2_SendBuffer(&u8g2);
 }
 
-//Slave oled display
-void ble_slave_oled(void) {
+// Slave oled display
+void ble_slave_oled(void)
+{
 	battery_percent = get_battery_level();
 
-	if (battery_percent != prev_battery_percent) {
+	if (battery_percent != prev_battery_percent)
+	{
 		u8g2_ClearBuffer(&u8g2);
 		u8g2_SetFont(&u8g2, u8g2_font_5x7_tf);
 		u8g2_DrawStr(&u8g2, 0, 6, GATTS_TAG);
@@ -203,40 +240,42 @@ void ble_slave_oled(void) {
 		char buf[sizeof(uint32_t)];
 		snprintf(buf, sizeof(uint32_t), "%d", battery_percent);
 		u8g2_DrawStr(&u8g2, 103 + offset_x_batt, 7 + offset_y_batt, "%");
-		if ((battery_percent < 100)
-				&& (battery_percent - prev_battery_percent >= 2)) {
+		if ((battery_percent < 100) && (battery_percent - prev_battery_percent >= 2))
+		{
 			u8g2_SetDrawColor(&u8g2, 0);
-			u8g2_DrawBox(&u8g2, 85  + offset_x_batt, 90 + offset_y_batt, 0, 7);
+			u8g2_DrawBox(&u8g2, 85 + offset_x_batt, 90 + offset_y_batt, 0, 7);
 			u8g2_SetDrawColor(&u8g2, 1);
 			u8g2_DrawStr(&u8g2, 90 + offset_x_batt, 7 + offset_y_batt, buf);
 			u8g2_SendBuffer(&u8g2);
 		}
-		if ((battery_percent < 100)
-				&& (battery_percent - prev_battery_percent >= 2)) {
+		if ((battery_percent < 100) && (battery_percent - prev_battery_percent >= 2))
+		{
 			erase_area(85 + offset_x_batt, 0 + offset_y_batt, 15, 7);
 			u8g2_DrawStr(&u8g2, 90, 7, buf);
 			u8g2_SendBuffer(&u8g2);
 		}
-		if ((battery_percent > 100) && (BATT_FLAG = 0)) {
+		if ((battery_percent > 100) && (BATT_FLAG = 0))
+		{
 			erase_area(85 + offset_x_batt, 0 + offset_y_batt, 15, 7);
 			u8g2_DrawStr(&u8g2, 85 + offset_x_batt, 7 + offset_y_batt, "100");
 			BATT_FLAG = 1;
 			u8g2_SendBuffer(&u8g2);
 		}
-		if (battery_percent == 100) {
+		if (battery_percent == 100)
+		{
 			erase_area(85 + offset_x_batt, 0 + offset_y_batt, 15, 7);
 			u8g2_DrawStr(&u8g2, 85 + offset_x_batt, 7 + offset_y_batt, "100");
 			u8g2_SendBuffer(&u8g2);
 		}
 		prev_battery_percent = battery_percent;
 	}
-
 }
 
-//Waiting for connecting animation
-void waiting_oled(void) {
-	char waiting[] = "Baglanti Bekleniyor ";
-	char conn[] = "Baglandi";
+// Waiting for connecting animation
+void waiting_oled(void)
+{
+	char waiting[] = "Baglanti \nBekleniyor ";
+	char conn[] = "Baglaniyor";
 
 #ifdef BATT_STAT
 	battery_percent = get_battery_level();
@@ -252,17 +291,21 @@ void waiting_oled(void) {
 	char buf[sizeof(uint32_t)];
 	snprintf(buf, sizeof(uint32_t), "%d", battery_percent);
 	u8g2_DrawStr(&u8g2, 103 + offset_x_batt, 7 + offset_y_batt, "%");
-	if ((battery_percent < 100)
-			&& (battery_percent - prev_battery_percent >= 2)) {
+	if ((battery_percent < 100) && (battery_percent - prev_battery_percent >= 2))
+	{
 		u8g2_DrawStr(&u8g2, 90 + offset_x_batt, 7 + offset_y_batt, buf);
 	}
-	if (battery_percent < 100) {
+	if (battery_percent < 100)
+	{
 		u8g2_DrawStr(&u8g2, 90 + offset_x_batt, 7 + offset_y_batt, buf);
-	} else {
+	}
+	else
+	{
 		u8g2_DrawStr(&u8g2, 85 + offset_x_batt, 7 + offset_y_batt, "100");
 	}
 
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < 3; i++)
+	{
 		u8g2_SetFont(&u8g2, u8g2_font_5x7_tr);
 		u8g2_DrawStr(&u8g2, 0, 15, waiting);
 		u8g2_DrawStr(&u8g2, 0, 30, conn);
@@ -270,11 +313,11 @@ void waiting_oled(void) {
 		vTaskDelay(100 / portTICK_PERIOD_MS);
 		strcat(conn, ".");
 	}
-
 }
 
-//shut down OLED
-void deinit_oled(void) {
+// shut down OLED
+void deinit_oled(void)
+{
 
 	u8g2_ClearBuffer(&u8g2);
 	u8g2_SetFont(&u8g2, u8g2_font_5x7_tf);
@@ -287,8 +330,9 @@ void deinit_oled(void) {
 	i2c_driver_delete(I2C_NUM_0);
 }
 
-//initialize oled
-void init_oled(const u8g2_cb_t *rotation) {
+// initialize oled
+void init_oled(const u8g2_cb_t *rotation)
+{
 
 	layer_recieve_q = xQueueCreate(32, sizeof(uint8_t));
 	led_recieve_q = xQueueCreate(32, sizeof(uint8_t));
@@ -299,25 +343,23 @@ void init_oled(const u8g2_cb_t *rotation) {
 	u8g2_esp32_hal.scl = OLED_SCL_PIN;
 	u8g2_esp32_hal_init(u8g2_esp32_hal);
 
-	if((rotation == DEG90) || rotation == DEG270){
+	if ((rotation == DEG90) || rotation == DEG270)
+	{
 
 		offset_x_batt = -85;
 		offset_y_batt = 120;
-
 	}
 
 	/* burayı değiştirdim
 	u8g2_Setup_ssd1306_i2c_128x64_noname_f(&u8g2, rotation,
 			u8g2_esp32_i2c_byte_cb, u8g2_esp32_gpio_and_delay_cb); // init u8g2 structure*/
 
-	
 	u8g2_Setup_ssd1306_i2c_128x32_univision_f(&u8g2, rotation,
-			u8g2_esp32_i2c_byte_cb, u8g2_esp32_gpio_and_delay_cb); // init u8g2 structure
+											  u8g2_esp32_i2c_byte_cb, u8g2_esp32_gpio_and_delay_cb); // init u8g2 structure
 
 	u8x8_SetI2CAddress(&u8g2.u8x8, 0x78);
-	u8g2_InitDisplay(&u8g2); // send init sequence to the display, display is in sleep mode after this,
+	u8g2_InitDisplay(&u8g2);	 // send init sequence to the display, display is in sleep mode after this,
 	u8g2_SetPowerSave(&u8g2, 0); // wake up display
 
 	u8g2_ClearBuffer(&u8g2);
-
 }
